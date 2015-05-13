14a80ef952e013ffcb0d3360e62155b959ee8866
nsuitter <nsuitter@saleforce.com>
2015-05-12 19:58:59 -0700

    Putting list of sites into an array and changing the loop to iterate though it instead of reading the WEBSITES file

--- blue_sharks/Benjamin_Huynh/Shell Script for Windows (Less Simple)/start_translation.sh
+++ blue_sharks/Benjamin_Huynh/Shell Script for Windows (Less Simple)/start_translation.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+
+sites=(
+http://www.alc.co.jp/
+http://jisho.org/
+http://ejje.weblio.jp/sentence/ 
+http://ejje.weblio.jp/sentence/ 
+http://www.wipo.int/wipopearl/search/linguisticSearch.html
+)
+
 start "" "./styleguide.docx"
-while read -r line; do
+for line in "${sites[@]}"; do
 	explorer "$line"
-done < WEBSITES.txt
+done
